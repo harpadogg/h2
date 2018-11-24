26d465c12381c128780c3e96e607a4b1e11b84b9
Harpa D Hafsteinsdottir <harpahafsteins@gmail.com>
2018-11-24 15:55:00 +0000

    villa löguð en js virkar ekki eins og það á að gera

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,5 +1,24 @@
-export default function empty(element) {
+export function empty(element) {
   while (element.firstChild) {
     element.removeChild(element.firstChild);
   }
+}
+
+export function el(name, className, ...children) {
+  const element = document.createElement(name);
+
+  if (className) {
+    element.classList.add(className);
+  }
+  if (Array.isArray(children)) {
+    children.forEach((child) => {
+      if (typeof child === 'string') {
+        element.appendChild(document.createTextNode(child));
+      } else if (child) {
+        element.appendChild(child);
+      }
+    });
+  }
+
+  return element;
 }
--- src/lib/list.js
+++ src/lib/list.js
@@ -2,8 +2,12 @@ import { empty } from './helpers';
 
 export default class List {
   constructor() {
+    this.button = document.querySelector('.button');
+    this.card = document.querySelector('.card');
     this.container = document.querySelector('.list');
     this.URL = '../lectures.json';
+
+    this.button.addEventListener('click', empty(this.container));
   }
 
   loadLectures() {
@@ -16,8 +20,60 @@ export default class List {
       });
   }
 
+  showLectures(data) {
+    empty(this.container);
+
+    const [{
+      title, category, thumbnail,
+    }] = data;
+
+    // const divList = document.createElement('div');
+    // divList.className = 'list';
+    const divRow = document.createElement('div');
+    divRow.className = 'grid__row';
+    const divCol = document.createElement('div');
+    divCol.className = 'grid__col';
+    const divCard = document.createElement('div');
+    divCard.className = 'card';
+    const divImage = document.createElement('div');
+    divImage.className = 'card__image';
+    const img = document.createElement('img');
+    img.className = 'card__img';
+    img.setAttribute('src', thumbnail);
+    const divText = document.createElement('div');
+    divText.className = 'card__text';
+    const subtitle = document.createElement('h3');
+    subtitle.className = 'card__subtitle';
+    subtitle.appendChild(document.createTextNode(category));
+    const cardTitle = document.createElement('h2');
+    cardTitle.className = 'card__title';
+    cardTitle.appendChild(document.createTextNode(title));
+
+    divText.appendChild(subtitle, cardTitle);
+    divImage.appendChild(img);
+    divCard.appendChild(divImage, divText);
+    divCol.appendChild(divCard);
+    divRow.appendChild(divCol);
+    this.container.appendChild(divRow);
+  }
+
   load() {
     this.loadLectures()
-      .then(data => console.log(data));
+      .then((data) => {
+        console.log(data);
+        this.showLectures(data.results);
+      })
+      .catch(() => {
+        console.error('Villa');
+      });
+
+    const savedData = window.localStorage.getItem(this.keyName);
+
+    if (savedData) {
+      const parsed = JSON.parse(savedData);
+      const date = new Date(parsed.date);
+
+      this.create(parsed.title, date);
+    }
   }
 }
