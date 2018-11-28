64826bab4e0cd2fdc4e50caa6ddf1373ced7e238
Harpa D Hafsteinsdottir <harpahafsteins@gmail.com>
2018-11-28 15:52:29 +0000

    held ad allt birtist a lecture nuna

--- src/lib/lecture.js
+++ src/lib/lecture.js
@@ -37,6 +37,7 @@ export default class Lecture {
     data.content.forEach((item) => {
       let youtube;
       let text;
+      let textArr;
       let quote;
       let code;
       let image;
@@ -45,19 +46,29 @@ export default class Lecture {
       let attribute;
       let figImg;
       let figCaption;
+      let pre;
 
       switch (item.type) {
         case 'youtube': youtube = el('iframe', 'lecure__video');
           youtube.setAttribute('src', item.data);
           this.main.appendChild(youtube); break;
-        case 'text': text = el('p', 'lecture__text', item.data);
-          this.main.appendChild(text); break;
+
+        case 'text': textArr = item.data.split('\n');
+          textArr.forEach((par) => {
+            text = el('p', 'lecture__text', par);
+            this.main.appendChild(text);
+          }); break;
+
         case 'quote': quote = el('blockquote', 'lecture__quote', item.data);
           attribute = el('cite', 'quote__cite', item.attribute);
           quote.appendChild(attribute);
           this.main.appendChild(quote); break;
+
         case 'code': code = el('code', 'lecture__code', item.data);
-          this.main.appendChild(code); break;
+          pre = el('pre');
+          pre.appendChild(code);
+          this.main.appendChild(pre); break;
+
         case 'image': figImg = el('figure', 'figure');
           image = el('img', 'lecture__image');
           image.setAttribute('src', item.data);
@@ -65,17 +76,27 @@ export default class Lecture {
           figImg.appendChild(image);
           figImg.appendChild(figCaption);
           this.main.appendChild(figImg); break;
+
         case 'list': list = el('ul', 'lecture__list');
           item.data.forEach((litem) => {
             const listItem = el('li', '.lecture__litem', litem);
             list.appendChild(listItem);
           });
           this.main.appendChild(list); break;
+
         case 'heading': heading = el('h2', 'lecture__heading', item.data);
           this.main.appendChild(heading); break;
+
         default: console.error('vantar case');
       }
     });
+    const footer = el('footer', 'lecture__footer')
+    const finish = el('button', 'lecture__button', 'Klára fyrirlestur');
+    footer.appendChild(finish);
+    const index = el('a', 'lecture__link', 'Til baka');
+    index.setAttribute('href', 'index.html');
+    footer.appendChild(index);
+    this.main.appendChild(footer);
   }
 
   load() {
